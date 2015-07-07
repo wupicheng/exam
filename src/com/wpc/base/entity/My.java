ef26e25cccbd7a43527782a3c74c8f9a0443fc9f
wupicheng <wupicheng@qq.com>
2015-07-07 20:38:08 +0800

    0.01

---
+++ src/com/wpc/base/entity/My.java
@@ -0,0 +1,12 @@
+package com.wpc.base.entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wupicheng
+ * Date: 15-7-7
+ * Time: 下午8:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class My {
+
+}
