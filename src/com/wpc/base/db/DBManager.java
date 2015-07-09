fa2c6b1b906daa963e4f6db1bcdbcba9c7b181a0
wupicheng <wupicheng@qq.com>
2015-07-09 14:52:50 +0800

    0.02实现登录和菜单

--- src/com/wpc/base/db/DBManager.java
+++ src/com/wpc/base/db/DBManager.java
@@ -11,7 +11,7 @@ import java.sql.*;
  */
 public class DBManager {
     static String driverName = "com.mysql.jdbc.Driver";
-    static String url = "jdbc:mysql://localhost:3306/exam";
+    static String url = "jdbc:mysql://203.195.207.109:3306/exam";
     static String user = "root";
     static String pwd = "root";
 
